51a1c726c2c0f5ce6c35dc4884bff19418bab930
Matthew Upchurch <upcheezy@gmail.com>
2020-05-21 22:44:00 -0400

    edit note is working now

--- src/EditNote/EditNote.jsx
+++ src/EditNote/EditNote.jsx
@@ -25,44 +25,18 @@ export default class EditNote extends Component {
             error: data.error,
           });
         } else {
-          data.forEach((element) => console.log(element));
-          //   console.log(data);
+          // console.log(data)
+          // console.log(this.props.match.params.note_id);
+          const newNote = data.find(
+            (note) => note.id === parseInt(this.props.match.params.note_id)
+          );
+          //   data.forEach((element) => console.log(element));
+          // console.log(newNote)
           this.setState({
-            name: data.name,
-            content: data.content,
-            folderId: data.folderId,
+            name: newNote.name,
+            content: newNote.content,
+            folderId: newNote.folderId,
           });
-        }
-      })
-      .catch((err) => {
-        this.setState({
-          error: err.message,
-        });
-      });
-
-    const foldersUrl = "http://localhost:8000/folders";
-    fetch(foldersUrl, {
-      method: "GET",
-      headers: {
-        "content-type": "application/json",
-        Authorization: `Bearer f3332591-addb-4571-b105-5165425549e6`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.error) {
-          this.setState({
-            error: data.error,
-          });
-        } else {
-          this.setState({
-            folders: data,
-          });
-          // const keyz = Object.keys(data).map(key => data[key].name);
-          // console.log(keyz);
-          // this.setState({
-          //   keyz
-          // })
         }
       })
       .catch((err) => {
@@ -72,23 +46,112 @@ export default class EditNote extends Component {
       });
   }
 
-  //   validateName() {
-  //     const name = this.state.nname.value.trim();
-  //     if (name.length === 0) {
-  //       return "Name is required";
-  //     } else if (name.length < 3) {
-  //       return "Name must be at least 3 characters long";
-  //     }
-  //     // console.log(name)
-  //   }
+  handleNameInput = (e) => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleContent = (e) => {
+    this.setState({ content: e.target.value });
+  };
+
+  updateFolderId(fid) {
+    const currentId = this.props.folders.find(({ name }) => name === fid);
+    this.setState({ folderId: currentId.id });
+    // console.log(currentId.id);
+  }
+
+  handleClickCancel = () => {
+    this.props.history.push("/");
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // get the form fields from the event
+    const { name, content, folderSelect } = e.target;
+    const note = {
+      note_name: name.value,
+      note_content: content.value,
+      folderId: folderSelect.value
+    };
+    this.setState({ error: null });
+    fetch(
+      `http://localhost:8000/notes/${this.props.match.params.note_id}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify(note),
+        headers: {
+          "content-type": "application/json",
+          authorization: `Bearer f3332591-addb-4571-b105-5165425549e6`,
+        },
+      }
+    )
+      .then((res) => {
+        if (!res.ok) {
+          // get the error message from the response,
+          return res.json().then((error) => {
+            // then throw it
+            throw error;
+          });
+        }
+      })
+      .then(() => {
+        this.props.history.push("/");
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ error });
+      });
+    //   console.log(this.context);
+  };
+
   render() {
+    // console.log(this.props);
+    const { content, name } = this.state;
     return (
-      <form className="EditNote__form">
+      <form className="EditNote__form" onSubmit={this.handleSubmit}>
         <div>
           <label htmlFor="name">Title</label>
-          <input type="text" />
-          {/* <ValidationError message={this.validateName()} /> */}
+          <input 
+            type="text" 
+            value={name} 
+            onChange={this.handleNameInput} 
+            name='name'
+            id='name'
+          />
+          <label htmlFor="note-content">Content</label>
+          <input 
+            type="text" 
+            value={content} 
+            onChange={this.handleContent} 
+            name='content'
+            id='content' 
+           />
+          <select
+            name="folderSelect"
+            id="folderSelect"
+            onChange={(e) => this.updateFolderId(e.target.value)}
+          >
+            <option value="none" selected hidden disabled>
+              folder name
+            </option>
+            {this.props.folders.map((folder) => {
+              return <option value={folder.name}>{folder.name}</option>;
+            })}
+          </select>
         </div>
+        <button
+          type="reset"
+          className="newFolder-button"
+          onClick={this.handleClickCancel}
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="newFolder-button"
+        >
+          Save
+        </button>
       </form>
     );
   }
